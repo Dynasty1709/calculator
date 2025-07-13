22e51f7652eef20b0834085b908c245666f532d4
Alan Wan <dynasty1709@gmail.com>
2025-07-13 01:39:22 -0700

    added more code

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+let num1 = 0;
+let num2 = 0;
+let result = 0;
+
+
 function add(num1, num2){
-    return num1 + num2
+    return num1 + num2;
 }
     
 function subtract(num1,num2){
@@ -12,4 +17,8 @@ function divide(num1,num2){
 
 function multiply(num1,num2){
     return num1 * num2
+}
+
+function operate(func,num1,num2){
+    return func(num1,num2)
 }
